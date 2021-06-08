4c6bc621321d923dc92593fda50c7ed62f9085d3
ATrossero <83141246+ATrossero@users.noreply.github.com>
2021-06-08 18:19:17 -0300

    Update main.cpp

--- TP1 Trossero/3b/TP 1 Trossero b/main.cpp
+++ TP1 Trossero/3b/TP 1 Trossero b/main.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-main(){
+main()
+{
     setlocale(LC_CTYPE, "Spanish");
     float m1,m2,b1,b2,y1,y2,x;
     cout << "Ingrese la pendiente de la recta 1" << endl;
     cin>> m1;
-    cout << "Ingrese el término independiente de la recta 1" << endl;
+    cout << "Ingrese el tÃ©rmino independiente de la recta 1" << endl;
     cin>> b1;
     cout << "Ingrese la pendiente de la recta 2" << endl;
     cin>> m2;
-    cout << "Ingrese el término independiente de la recta 2" << endl;
+    cout << "Ingrese el tÃ©rmino independiente de la recta 2" << endl;
     cin>> b2;
-    x=(b2-b1)/(m1-m2);
-    y1=(m1*x)+b1;
-    y2=(m2*x)+b2;
-    if (m1==m2)
+    if ((m1-m2)==0)
     {
-        if (b1==b2){
-            cout<< "Las rectas son iguales, se intersecan en todos sus puntos" <<endl;
-                    }else{
-                        cout<< "Las rectas son paralelas" <<endl;
-                        }
-    }else{
-        if (y1==y2){
-            cout<< "Las rectas se intersecan en las coordenadas: ("<<x<<";"<<y1<<")"<< endl;
-                    }
-        }
+         if(b1==b2)
+            {
+                cout<< "Las rectas son iguales, se intersecan en todos sus puntos" <<endl;
+            }else{
+                cout<< "Las rectas son paralelas, no hay intersecciÃ³n." <<endl;
+            }
+
+    }else
+    {
+        x=(b2-b1)/(m1-m2);
+        y1=(m1*x)+b1;
+        y2=(m2*x)+b2;
+        if (y1==y2)
+                {
+                cout<< "Las rectas se intersecan en las coordenadas: ("<<x<<";"<<y1<<")"<< endl;
+                }
+    }
     return 0;
 }
